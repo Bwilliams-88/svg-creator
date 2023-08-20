4b1960ed5cc6bb8c17475c0478579e5963219d2a
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-08-20 12:54:13 -0500

    initial commit, had issues with previous repo

---
+++ generatedSVG.js
@@ -0,0 +1,7 @@
+// Function to render shapes
+
+// Function to render text
+
+// Function to rend background color of logo
+
+module.exports = generatedSVG;
---
+++ index.js
@@ -0,0 +1,61 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
+const svg = require('generatedSVG.js');
+
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to 3 characters for your logo text (example: MIT or IBM).',
+    },
+    {
+        type: 'input',
+        name: 'txtcolor',
+        message: 'What color text would you like?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like your logo to be?',
+        choices: ['Square', 'Triangle', 'Circle'],
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Enter a keyword or hexadecimal number for your background color. (example: Yellow or #FFFF00).',
+    } 
+];
+
+// Function to create the logo.svg file
+function writeSVG(content) {
+    console.log(content);
+    const svgContent = svg(content);
+    console.log(svgContent);
+
+    fs.writeFile('logo.svg', svgContent, (err) => {
+        if (err) {
+            console.error('Error writing logo.svg:', err);
+        } else {
+            console.log('Generated logo.svg');
+        }
+    });
+};
+
+// Function to initialize app
+function init() {
+    console.log('Application Initialized');
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        writeSVG(answers);        
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+};
+
+// Function call to initialize app
+init();
